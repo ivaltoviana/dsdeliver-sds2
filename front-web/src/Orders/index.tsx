dfb924ba19dc474b8a13f5440797f4cfae14017a
Ivalto Viana <ivaltovianna@gmail.com>
2021-01-10 00:25:18 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,25 +1,30 @@
-import { useEffect, useState } from 'react';
-import './styles.css';
-import StepsHeader from './StepsHeader';
-import ProductsList from './ProductsList';
-import { Product } from './types';
-import { fetchProducts } from '../api';
+import { useEffect, useState } from "react";
+import "./styles.css";
+import StepsHeader from "./StepsHeader";
+import ProductsList from "./ProductsList";
+import { OrderLocationdata, Product } from "./types";
+import { fetchProducts } from "../api";
+import OrderLocation from "./OrderLocation";
 
 function Orders() {
-    const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
 
-    useEffect(() => {
-        fetchProducts()
-        .then(response => setProducts(response.data))
-        .catch(error => console.log(error))
-    }, []);
+  useEffect(() => {
+    fetchProducts()
+      .then(response => setProducts(response.data ))
+      .catch(error => console.log(error));
+  }, []);
 
-    return(
-        <div className="orders-container">
-            <StepsHeader />
-            <ProductsList products={products} />
-        </div>
-    )
+  return (
+    <div className="orders-container">
+      <StepsHeader />
+      <ProductsList products={products} />
+      <OrderLocation
+        onChangeLocation={(location) => setOrderLocation(location)}
+      />
+    </div>
+  );
 }
 
 export default Orders;
